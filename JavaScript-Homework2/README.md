3fac00cf9da0ea9d9429ceea124cb34258c11412
Ege KARA <karaege98@gmail.com>
2022-10-17 22:26:22 +0300

    Second Homework for JavaScript

---
+++ JavaScript-Homework2/README.md
@@ -0,0 +1,16 @@
+# To Do List with JavaScript
+## [Patika.dev](patika.dev) JavaScript Course Second Homework
+---
+In this project [patika.dev](patika.dev) provided us with the HTML and CSS files for a To Do List Web App and gave us some tasks to implement some required parts to this project using JavaScript. Those tasks are listed below.
+
+### Tasks
+- Make it impossible to add empty tasks to the list
+- Create a function for:
+    1. Adding a task to the list
+    2. Removing a task from the list
+    3. Marking a task as complete
+- Using Bootstrap's Toast make alerts appear for:
+    1. Attempting to add an empty task to the list
+    2. Successfully adding a task to the list
+---
+After finishing this project I can confidently say that this project improved my understanding of Document Object Model(DOM) and how to use them.
---
+++ JavaScript-Homework2/script.js
@@ -0,0 +1,54 @@
+const ADD_BTN = document.querySelector("#liveToastBtn");    // Button next to the input field
+const TO_DO_LIDOM = document.querySelector("#list");        // The ul element
+
+let removeBtns = document.getElementsByClassName("remove"); // Buttons with remove class. These buttons are used for removing items from the list
+let myToast, bStrapAlert;                                   // Variables for toast
+
+ADD_BTN.addEventListener("click", newElement);              // Event listener for the button next to the input field
+
+// Function to add a new element to the list
+function newElement(){                                      
+    let taskToDo = document.querySelector("#task").value;   // Get the value of input
+    if (taskToDo.trim() != "") {                            // Check if it's empty. trim() helps with " " string
+        let liDOM = document.createElement(`li`);           // Create a li element
+        liDOM.className = `list-element `;                  // Give it a class
+        // Set the innerHTML using template literal
+        liDOM.innerHTML = ` 
+        ${taskToDo} 
+        <span onclick="removeElement(this)" class="btn remove">x</span>
+        `;
+        liDOM.onclick = function(){taskDone(this)};         // Give the created element a onclick function with a value so that taskDone can be called on click
+        myToast = document.querySelector(".toast.success"); // Get the DOM with toast and success classes 
+        bStrapAlert = new bootstrap.Toast(myToast);         // Define the toast
+        bStrapAlert.show();                                 // Show the toast
+        TO_DO_LIDOM.append(liDOM);                          // Append to the ul element
+        resetInput();                                       // Reset the value of input
+    } else {                                                // If it's empty
+        myToast = document.querySelector(".toast.error");   // Get the DO with toast and error classes
+        bStrapAlert = new bootstrap.Toast(myToast);         // Define the toast
+        bStrapAlert.show();                                 // Show the toast
+    }
+    
+}
+// Function to reset input
+function resetInput(){
+    let input = document.querySelector("#task");            // Get the input
+    input.value = "";                                       // Reset the value of that input by making it an empty string
+}
+// Function to remove the element. Takes the button as its parameter
+function removeElement(btn){                                 
+    let liElement = btn.parentElement;                      // Button is the child of a li element so we can learn which element will be removed with this
+    TO_DO_LIDOM.removeChild(liElement);                     // Using the DOM of the ul element we can remove the li element which is its child
+}
+// Function for user to indicate that a task is done. Takes the li element as its parameter. This proccess can be taken back as a solution of a missclick
+// Background color becomes green and text color becomes white if the task is done
+function taskDone (item){
+    let defaultStyle = item.style;                          // Stores the original style values of the item in a variable
+    
+    if (item.style.color == "white") {                      // Checks if the current text color is white. If it's white it means we changed it and clicking back means we want to change it back
+        item.style = defaultStyle;                          // If it's different it means the user previously marked that task as done. Changes it back
+    } else {
+        item.style.backgroundColor = "#04AA6D";             // Makes the background green(#04AA6D) 
+        item.style.color = "white";                         // Makes the text white
+    }
+}
